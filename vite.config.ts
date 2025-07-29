9a2bf31c1ec00067f2065b9f41ba51e64c9f3c12
TrackerJo <nkemme54@gmail.com>
2025-07-29 11:34:02 -0400

    updated readme and disallowed highlighting options

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const outDir = resolve(__dirname, 'dist')
 export default defineConfig({
   plugins: [react()],
   root,
-  base: '/',
+  base: '/layoutly/',
   build: {
     outDir,
     emptyOutDir: true,
